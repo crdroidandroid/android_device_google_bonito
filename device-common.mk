9d1e79bbd166a533a4a1839a22eb7073b3e8cc4a
Ivy <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2025-08-19 12:47:14 -0230

    common: nuke gcam aperature is preferred for crdroid

--- device-common.mk
+++ device-common.mk
@@ -24,9 +24,6 @@ PRODUCT_PLATFORM := sdm670
 include device/google/bonito/device-audio-mfg.mk
 include device/google/bonito/device.mk
 
-#Ship GCam
-$(call inherit-product, vendor/Camera/b4s4/config.mk)
-
 # Device now uses mini_gapps
 TARGET_USES_MINI_GAPPS := true
 
